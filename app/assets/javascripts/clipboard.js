471c3f96be20b5a632d0ea47a35d79de31a4fe01
Conor Starrs <conor.starrs@gmail.com>
2011-10-22 15:18:18 +0100

    Trying to get javascript to work on Heroku

--- app/assets/javascripts/clipboard.js
+++ app/assets/javascripts/clipboard.js
@@ -1,3 +1,3 @@
-function copyToClipboard (text) {
-  window.prompt ("Copy to clipboard: Ctrl+C, Enter", text);
+function copyToClipboard (a) {
+  window.prompt ("Copy to clipboard: Ctrl+C, Enter",a);
 }
