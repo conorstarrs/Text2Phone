887fc73b45d55900993ddf4101321d8e721e10e5
Conor Starrs <conor.starrs@gmail.com>
2011-10-22 16:39:55 +0100

    More view changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,9 @@ class UsersController < ApplicationController
     @user_email = current_user.email
     @user_last_signin = current_user.last_sign_in_at
   end
+  
+  def info
+    @user_email = current_user.email
+    @user_last_signin = current_user.last_sign_in_at
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def striped_tr(*my_strings)
+    rv="<tr class=#{stripe}>\n".html_safe
+    my_strings.each do |a_string|
+      rv << "  <td>#{h a_string || ''}</td>\n".html_safe
+    end
+    rv << "</tr>\n".html_safe
+    return rv
+  end
 end
