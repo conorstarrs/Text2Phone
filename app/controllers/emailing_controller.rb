1e284bff36e7526a447a1c0043657475c7116a89
Conor Starrs <conor.starrs@gmail.com>
2011-10-31 22:01:09 +0000

    Emailer effort

--- app/controllers/emailing_controller.rb
+++ app/controllers/emailing_controller.rb
@@ -1,11 +1,23 @@
 class EmailingController < ApplicationController
   def index
+    @text = Text.find(params[:text])
+    @email = current_user.email
   end
 
-  def create
-    EmailText.send_by_email.deliver
+  def email_me
+    @user = current_user
+    @text = Text.find(params[:text])
+    EmailText.email_me(@user, @text).deliver
     flash[:notice] = "Your message has been sent."
-    redirect_to root_path
+    redirect_to :controller => 'texts', :action => "index"
   end
-
+  
+  def email_friend
+    @friend_email = params["friend"]
+    @user = current_user
+    @text = Text.find(params[:text])
+    EmailText.email_friend(@friend_email, @user, @text).deliver
+    flash[:notice] = "Your message has been sent."
+    redirect_to :controller => 'texts', :action => "index"
+  end
 end
--- app/mailers/email_text.rb
+++ app/mailers/email_text.rb
@@ -1,9 +1,17 @@
 class EmailText < ActionMailer::Base
-  def send_by_email
-      recipients "conor.starrs@gmail.com"
-      from       "My Forum "
-      subject    "Please activate your new account"
+  def email_me(user, text)
+      recipients "#{user.email}"
+      from       "Text2Phone"
+      subject    "You have recieved an e-mail from Text2Phone"
       sent_on    Time.now
-      body       "Hello there!"
+      body       text.text
+  end
+  
+  def email_friend(friend_email, user, text)
+      recipients "#{friend_email}"
+      from       "Text2Phone"
+      subject    "#{user.email} has sent you an e-mail from Text2Phone"
+      sent_on    Time.now
+      body       text.text
   end
 end
